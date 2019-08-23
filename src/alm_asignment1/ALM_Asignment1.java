0919f19cf218f7350dd73a2c4b7527881dcf934e
Adam Bäckström <aob.backstrom@gmail.com>
2019-08-23 11:01:19 +0200

    Merge conflict

--- src/alm_asignment1/ALM_Asignment1.java
+++ src/alm_asignment1/ALM_Asignment1.java
@@ -7,13 +7,15 @@ public class ALM_Asignment1 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in, "iso8859-1");
-        //Converter converter = new Converter();
+        Converter converter = new Converter();
 
         System.out.println("Welcome to the conversion space! \nSelect what you would like to convert by entering 1 or 2:"
                 + "  \n1. Fahrenheit to celcius \n2. Kilometers to knots");
 
         String mode = sc.nextLine();
 
+        //Merge Conflict
+        
         System.out.println(mode);
     }
 
