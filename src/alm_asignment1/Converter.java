ed76af4c5872493a54cc5555ec21b79ad79f868d
Adam Bäckström <aob.backstrom@gmail.com>
2019-08-23 10:56:04 +0200

    Created celsius to fahrenhet method

--- src/alm_asignment1/Converter.java
+++ src/alm_asignment1/Converter.java
@@ -7,5 +7,12 @@ public class Converter {
         double celsius = (fahrenheit -32) / 1.8;
         
         return celsius;
-    }   
+    }
+    
+    public double cToF(double celsius) {
+        
+        double fahrenheit = celsius * 1.8 + 32;
+        
+        return fahrenheit;
+    }
 }
